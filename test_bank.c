0cbeec653de7b81373dd2e548958557b295b742b
sharpscar <sharpscar@gmail.com>
2025-03-22 00:31:11 +0900

    맥북 숫자이슈일까 내 지능이슈일까

--- test_bank.c
+++ test_bank.c
@@ -113,14 +113,15 @@
 //     struct refinery_material rm;
 // };
 
- 
-
+struct inven sword_store_to_bank(struct inven, struct inven);
+int get_index_for_inven_sword_array(struct inven inven_);
 int main()
 {   
     srand(time(NULL));
     setting();
 
     // 일단 인벤에 칼 2자루와 갑옷 1개를 임의로 넣어보자
+    struct inven bank_inven;
     struct inven my_inven;
     struct sword sword1_basic;
     struct sword sword3_japanese;
@@ -131,100 +132,17 @@ int main()
     sword3_japanese= set_sword(sword3_japanese,"일본도", 20,0,0,1000,0,0);
     sword6_world_Best= set_sword(sword6_world_Best,"천상천하제일무쌍도", 100,0,0,0,0,0);    
 
+    //임의로 가짜데이터를 넣어봄 - uid는 잡아야할 버그: 중복된 유니크키?
     my_inven.sw[0] = sword1_basic;
     my_inven.sw[1] = sword3_japanese;
     my_inven.sw[2] = sword6_world_Best;
     // 가방의 더 넣을수 있는 인덱스 (5- index가 1보다 클때 구매가능)
-    int inven_space_index_sword,answer;
-    int sw_cnt  = sizeof(my_inven.sw) /sizeof(struct sword);
-    int index=0;
-    while(index<20)
-    {
-        if(my_inven.sw[index].uid <=0)
-        {
-            inven_space_index_sword = index;
-            break;
-        }
-        index++;
 
-    }
-    //쓰고 있는 크기를 가져와서 칼의 경우는 5- inven_space_index = 더 넣을수 있는 공간입니다.
-
-
-    /**
-     * 세터 함수를 못씀 ㅋㅋㅋㅋㅋ 
-struct inven 인벤 자료형이다.
-{   my_inven.sw[i].uid <0
-    int money;
-    struct sword sw[5];
-    struct amor am[15];
-    struct consume con[1200];    
-    struct refinery_material rm[100];
-};
-    strlen
-
-sizeof(my_inven.sw) 의 크기만큼 반복문을 돌려서 uid가 -1 을 찾아본다. 그리고 그 인덱스를 찾아서 배열의 크기를 가늠할
-
-
-     */
-    //inven_space_index 칼의 남은공간을 
-    // 현재 칼이 있는지 체크 해서 배열크기를 가져오고 넣을수 있는 인덱스를 가져와야함 .
-    printf("현재 내 가방에는 칼이 %d개 있다.\n",inven_space_index_sword);
-    for(int i=0; i<inven_space_index_sword; i++)
-    {
-        // 출력한다.칼의 이름을 출력한다.
-        printf("%s  칼이 있습니다. 꺼내실 장비를 선택해줘요 %d 번\n", my_inven.sw[i].name, i );
-
-    }
+    bank_inven =  sword_store_to_bank(bank_inven,my_inven);
     
-    scanf("몇번을 꺼내시겠습니까? %d ", &answer);
-    /**7: void deleteStudent( student* aryStudent, int nStudentCount )  // 학생 정보 삭제
- {
-         int nDelStudent; //사용자 입력값
-
-         printf( "몇번째 학생을 삭제하실 건가요?\n" );
-         scanf( "%d", &nDelStudent );
-
-         // for 반복문의 i는 구조체 배열의 인덱스로 사용됨을 상기하시기 바랍니다^^
-         for( int i=nDelStudent-1; i<nStudentCount-1; i++ )
-         {
-                 memcpy( &aryStudent[i], &aryStudent[i+1], sizeof( student ) );
-                 // memcpy()함수는 두번째 인자에서 첫번째 인자의 주소로
-                 // 세번째 인자의 크기만큼
-                 // 메모리를 복사하는 함수입니다.
-                 // 즉, 학생의 정보를 뒤에서 앞으로 복사하는 것이지요.
-                 // 자세한 사항은 MSDN을 참고하시기 바랍니다^^;
-         }
- }
-
-
-     * 
-     * 
-     * 
-     */
-
-
     
-
-
-
-    // 그리고 그것들 모두를 출력
-
-    // 그것들중 갑옷 1개를 맡겨보자
-
-
-
-    // 돈은 우선 10000원으로 초기화하자 
-
-    // 맡겨뒀던 갑옷1개를 찾아보자
-
-
-
-
-
-    return 0;
+return 0;
 }
-
 struct inven bank_main(struct inven user_inven)
 {
     int answer;
@@ -244,12 +162,7 @@ struct inven bank_main(struct inven user_inven)
         // 만약 보관소의 물품의 크기가 100개가 넘으면 더이상 보관이 안된다는 메시지를 한다.
         
         // 유저의 인벤에서 물건들을 뿌려준다. 
-        
-        //이중에서보관하실 물건을 골라주세요
-
-        //1,2,3,4,5,6,7,8,9
-
-        //1,2,3,4,5,6,7,8,9
+        // 물품 보관함수 call
 
 
     }else if(answer == 2)
@@ -263,3 +176,87 @@ return user_inven;
     
     // 우선 유저의 인벤토리를 불러온다.
 }
+
+
+struct inven store_sword_to_banks_inven
+(struct inven user_inven, struct inven bank_inven)
+{
+    //유저 인벤을 매개변수로 받아서 선택한 칼의 인덱스를
+    //이용해서 해당 칼 정보를 가져온다
+    // 가져온 정보를 은행의 인벤에 삽입
+    // 은행 인벤의 sw배열_마지막 빈배열 위치를 조회  
+    //  삽입  << 여기까지 저장
+    
+
+    // 유저의 인벤 구조체안의 칼(구조체) 배열에서 삭제 ㅎㅎ
+    // 삭제하는 코드
+    /*
+    i는 삭제해야할 인덱스다 cnt는 해당 배열의 크기
+    for(int j=i; j<cnt-1; j++)
+    {
+        list[j] = list[j+1];
+    }
+    */
+
+return bank_inven;
+
+}
+
+    struct inven sword_store_to_bank(struct inven bank_inven, struct inven user_inven)
+    {
+        int inven_space_index_sword;
+
+
+        int sw_cnt  = sizeof(user_inven.sw) /sizeof(struct sword);
+        int index=0;
+        int bank_last_index=0;
+        int answer=0;
+
+        // get_index_for_inven_sword_array :끝 인덱스를 가져오는 함수 없으면 0 꽉차면 5겠지?
+        inven_space_index_sword = get_index_for_inven_sword_array(user_inven);
+        //쓰고 있는 크기를 가져와서 칼의 경우는 5- inven_space_index = 더 넣을수 있는 공간입니다.
+
+        //inven_space_index 해당 칼이 있는 인덱스  없으면 
+        // 현재 칼이 있는지 체크 해서 배열크기를 가져오고 넣을수 있는 인덱스를 가져와야함 .
+        printf("현재 내 가방에는 칼이 %d개 있다.\n",inven_space_index_sword);
+        for(int i=0; i<inven_space_index_sword; i++)
+        {
+            // 출력한다.칼의 이름을 출력한다.
+            printf("%s  칼이 있습니다. 꺼내실 장비를 선택해줘요 %d 번\n", user_inven.sw[i].name, i+1 );
+
+        }
+        
+        scanf("몇번을 꺼내시겠습니까? %d ", &answer);
+        // answer = 2;
+        printf("=====================> %d\n" , answer);
+        // struct sword selected_sword = user_inven.sw[answer]; //정보를 넣는다.
+
+        printf("%s", user_inven.sw[answer].name);
+        //은행 인벤에 sw 리스트의 인덱스를 가져온다. 
+        bank_last_index = get_index_for_inven_sword_array(bank_inven);
+
+
+
+
+
+
+        return bank_inven; //bank_inven;
+    }
+
+   int get_index_for_inven_sword_array(struct inven inven_)
+    {
+        int index= 0 ;
+        int inven_space_index_sword=0;
+        while(index<20)
+        {
+            // 빈배열 찾는방식
+            if(inven_.sw[index].uid <=0)
+            {
+                inven_space_index_sword = index;
+                break;
+            }
+            index++;
+
+        }
+        return inven_space_index_sword;
+    }
